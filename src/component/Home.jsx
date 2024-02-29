a0a2fd665991df136d1f2daa5b152b90ccff49ea
Dipak-Gautam <anjangautam095@gmail.com>
2024-02-29 15:35:09 +0545

    testing version update

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Main component or home component</div>;
+  return <div>Main component or home component hello</div>;
 };
 
 export default Home;
