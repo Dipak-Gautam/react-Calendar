efd9d778a1f589b1dffec8b251a5bac09b2c9b9c
Dipak-Gautam <anjangautam095@gmail.com>
2024-03-01 18:21:55 +0545

    Some changes in calander

--- src/component/DateConveter.jsx
+++ src/component/DateConveter.jsx
@@ -1,7 +1,38 @@
 import React from "react";
 
 const DateConveter = () => {
-  return <div>DateConveter</div>;
+  return (
+    <div className="mx-12 flex my-12">
+      <div className="w-full flex ">
+        <div className="w-[48%] flex flex-col items-center border-black border m-3 rounded-2xl overflow-hidden text-xl">
+          <div className="bg-[#8f3771] text-white p-3 w-full flex flex-col items-center">
+            <h1>AD to BS date conveter</h1>
+            <p>(BS range from 2000 -2999)</p>
+          </div>
+          <div className="p-3 text-xl w-[92%] flex flex-col items-center">
+            <p>Enter date in Bs(YYYY-MM-DD)</p>
+            <input
+              type="date"
+              className="w-[100%] text-center border-gray-900 border rounded-md m-3 p-1"
+            />
+          </div>
+        </div>
+        <div className="w-[48%] flex flex-col items-center border-black border m-3 rounded-2xl overflow-hidden text-xl">
+          <div className="bg-[#8f3771] text-white p-3 w-full flex flex-col items-center">
+            <h1>Bs to AD date conveter</h1>
+            <p>(AD range from 1944 -2043)</p>
+          </div>
+          <div className="p-3 text-xl w-[92%] flex flex-col items-center">
+            <p>Enter date in Bs(YYYY-MM-DD)</p>
+            <input
+              type="date"
+              className="w-[100%] text-center border-gray-900 border rounded-md m-3 p-1"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default DateConveter;
--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,50 +1,93 @@
 import React from "react";
-
+import { MdOutlineNavigateNext } from "react-icons/md";
+import { GrFormPrevious } from "react-icons/gr";
 const Home = () => {
   const time = new Date();
   return (
     <>
-      <div className="mx-12">
-        <div className="w-full flex flex-col items-center p-10 text-2xl font-bold text-slate-600">
-          <h1>Today's Date:</h1>
-          <h1> {time.toDateString()}</h1>
+      <div className="mx-12 flex">
+        <div className="w-[30%] border-2 border-red-600">
+          <div className="w-full flex flex-col items-center p-10 text-2xl font-bold text-slate-600">
+            <h1>Today's Date:</h1>
+            <h1 className="underline"> {time.toDateString()}</h1>
+          </div>
         </div>
 
-        <div className="flex w-full h-auto">
-          {/* dateconveter start here */}
+        {/* clander starts here */}
 
-          <div className="w-[40%] border-2 border-red-500 items-center">
-            <div className="w-[92%] flex flex-col items-center border-black border m-3 rounded-2xl overflow-hidden text-xl">
-              <div className="bg-[#8f3771] text-white p-3 w-full flex flex-col items-center">
-                <h1>AD to BS date conveter</h1>
-                <p>(BS range from 2000 -2999)</p>
+        <div className="border-2 border-red-700 w-[70%] p-2">
+          <div className="flex w-full h-auto flex-col">
+            {/* calinder heading */}
+
+            <div className="flex justify-between my-4 text-3xl font-semibold text-slate-600">
+              <div className="flex">
+                <h1 className="">Falgun</h1>
+                <h1 className="mx-3">2024</h1>
               </div>
-              <div className="p-3 text-xl w-[92%] flex flex-col items-center">
-                <p>Enter date in Bs(YYYY-MM-DD)</p>
-                <input
-                  type="date"
-                  className="w-[100%] text-center border-gray-900 border rounded-md m-3 p-1"
-                />
+              <div>
+                <button>
+                  <GrFormPrevious className="bg-[#832161] text-white h-full mb-0" />
+                </button>
+                <button>
+                  <MdOutlineNavigateNext className="bg-[#832161] text-white h-full mb-0" />
+                </button>
               </div>
             </div>
-            <div className="w-[92%] flex flex-col items-center border-black border m-3 rounded-2xl overflow-hidden text-xl">
-              <div className="bg-[#8f3771] text-white p-3 w-full flex flex-col items-center">
-                <h1>Bs to AD date conveter</h1>
-                <p>(AD range from 1944 -2043)</p>
+
+            {/* calinder heading ends */}
+
+            {/* actualcalinderstarts here */}
+
+            <div>
+              <div className="grid grid-cols-7 text-center font-bold h-9 ">
+                <div className="border-2 border-black">Sun </div>
+                <div className="border-2 border-black">Mon </div>
+                <div className="border-2 border-black">Tue </div>
+                <div className="border-2 border-black">Wed </div>
+                <div className="border-2 border-black">Thu </div>
+                <div className="border-2 border-black">Fri </div>
+                <div className="border-2 border-black text-red-600">Sat</div>
               </div>
-              <div className="p-3 text-xl w-[92%] flex flex-col items-center">
-                <p>Enter date in Bs(YYYY-MM-DD)</p>
-                <input
-                  type="date"
-                  className="w-[100%] text-center border-gray-900 border rounded-md m-3 p-1"
-                />
+              {/* dates below */}
+              <div className="grid grid-cols-7 text-center font-bold items-center">
+                <div className="h-24 border-2 border-black"></div>
+                <div className="h-24 border-2 border-black"></div>
+                <div className="h-24 border-2 border-black"></div>
+                <div className="h-24 border-2 border-black"></div>
+                <div className="h-24 border-2 border-black"></div>
+                <div className="h-24 border-2 border-black"></div>
+                <div className="h-24 border-2 border-black"></div>
+                <div className="h-24 border-2 border-black"></div>
+                <div className="h-24 border-2 border-black"></div>
+                <div className="h-24 border-2 border-black"></div>
+                <div className="h-24 border-2 border-black"></div>
+                <div className="h-24 border-2 border-black"></div>
+                <div className="h-24 border-2 border-black"></div>
+                <div className="h-24 border-2 border-black"></div>
+                <div className="h-24 border-2 border-black"></div>
+                <div className="h-24 border-2 border-black"></div>
+                <div className="h-24 border-2 border-black"></div>
+                <div className="h-24 border-2 border-black"></div>
+                <div className="h-24 border-2 border-black"></div>
+                <div className="h-24 border-2 border-black"></div>
+                <div className="h-24 border-2 border-black"></div>
+                <div className="h-24 border-2 border-black"></div>
+                <div className="h-24 border-2 border-black"></div>
+                <div className="h-24 border-2 border-black"></div>
+                <div className="h-24 border-2 border-black"></div>
+                <div className="h-24 border-2 border-black"></div>
+                <div className="h-24 border-2 border-black"></div>
+                <div className="h-24 border-2 border-black"></div>
+                <div className="h-24 border-2 border-black"></div>
+                <div className="h-24 border-2 border-black"></div>
+                <div className="h-24 border-2 border-black"></div>
+                <div className="h-24 border-2 border-black"></div>
+                <div className="h-24 border-2 border-black"></div>
+                <div className="h-24 border-2 border-black"></div>
+                <div className="h-24 border-2 border-black"></div>
               </div>
             </div>
           </div>
-
-          {/* dateconveter ends here */}
-
-          <div>calander</div>
         </div>
       </div>
     </>
